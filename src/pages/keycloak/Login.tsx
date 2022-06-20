b1f40c71872fb39256e873571891ca66eaddb46a
Kim JinHo <skulter@naver.com>
2022-06-20 12:15:20 +0900

    테스트

--- src/pages/keycloak/Login.tsx
+++ src/pages/keycloak/Login.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useRef, memo, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import styled from 'styled-components'; 
-import { Button, TextField } from '@mui/material';
-
+import styled from 'styled-components';
+import { Box, Button, Grid, TextField } from '@mui/material';
 import type { KcProps } from 'keycloakify/lib/components/KcProps';
 import type { KcContextType } from '@/utils/keycloakManager';
 
@@ -10,24 +9,30 @@ import bg from '@/assets/images/login-bg.jpg';
 
 type KcContext_Login = Extract<KcContextType, { pageId: 'login.ftl' }>;
 
-const StyledLogin = styled.div`
+const StyledLogin = styled(Grid)`
   min-width: 100vw;
   min-height: 100vh;
-  background-image: url(${bg});
+  /* background-image: url(${bg});
   background-size: cover;
-  background-repeat: no-repeat;
+  background-repeat: no-repeat; */
+  background-color: rgb(222, 230, 240);
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
 
+const LoginFormContainer = styled(Grid)`
+    box-shadow: 0 0 6px 2px grey;
+    border-radius: 5px;
+    background-color: white;
+    justify-content: center;
+    width: 25rem;
+`
+
 const LoginForm = styled.form`
   width: 25rem;
-  height: 15rem;
-  background-color: white;
-  border-radius: 5px;
-  box-shadow: 2px 2px 8px 0px;
+  height: 20rem;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -40,14 +45,22 @@ const LoginInput = styled(TextField)`
 `;
 
 const LoginButton = styled(Button)`
-  width: 20rem;
+  background-color: rgb(28, 47, 68)  !important;
+  height:2rem;
+  width: 100%;
 `;
 
+const SocialForm = styled('form')`
+border: 1px solid;
+background-color: white;
+  width: 20rem;
+`
+
 export const Login = memo(
-	({ kcContext, ...props }: { kcContext: KcContext_Login } & KcProps) => {
+  ({ kcContext, ...props }: { kcContext: KcContext_Login } & KcProps) => {
     const { t } = useTranslation();
     const form = useRef<HTMLFormElement>(null);
-		const { social, url, message, realm, } = kcContext;
+    const { social, url, message, realm, } = kcContext;
     const isSessionOut = message?.summary.includes('attempt timed out') || message?.summary.includes('Timeout');
 
     console.log(kcContext);
@@ -56,6 +69,18 @@ export const Login = memo(
       console.log(form);
       form?.current?.submit();
     };
+    const test = {
+      displayInfo: true,
+      providers: [
+        {
+          displayName: "Google",
+          providerId: "google",
+          loginUrl: "/auth/realms/test/broker/google/login?client_id=account-console&tab_id=0ncS40QMm5Q&session_code=OacQkf8KlbI6tefn7E5pDiTte7MTKHE5Qlenk1IWp5o",
+          iconClasses: "fa fa-google",
+          alias: "google"
+        }
+      ]
+    }
 
     useEffect(() => {
       if (message?.summary === 'emailSentMessage') {
@@ -67,27 +92,52 @@ export const Login = memo(
       }
     }, []);
 
-		return (
-      <StyledLogin>
-        <LoginForm ref={form} method="post" action={url.loginAction}>
-          <LoginInput
-            id="username"
-            name="username"
-            size="small"
-            label={t('id')}
-          />
-          <LoginInput
-            label={t('password')}
-            id="password"
-            name="password"
-            type="password"
-            size="small"
-          />
-          <LoginButton variant="contained" onClick={() => handleSubmit()}>{ t('login') }</LoginButton>
-        </LoginForm>
-      </StyledLogin>
+    return (
+      <StyledLogin container>
+        <LoginFormContainer item>
+          <LoginForm ref={form} method="post" action={url.loginAction}>
+            {/* <Box sx={{ width: '60px' }}>
+            {test.providers?.map(provider => (
+              <SocialForm method="post" action={provider.loginUrl}>
+                <Button type='submit'> <i className={provider.iconClasses} aria-hidden="true"></i>{provider.displayName} 로그인</Button>
+              </SocialForm>
+            ))}
+          </Box> */}
+            <Grid item direction='column' sx={{ alignItems: 'center' }}>
+              <Grid item>
+                <LoginInput
+                  id="username"
+                  name="username"
+                  size="small"
+                  label='Email'
+                />
+              </Grid>
+              <Grid item>
+                <LoginInput
+                  id="password"
+                  name="password"
+                  type="password"
+                  size="small"
+                  label='password'
+                />
+              </Grid>
+              <LoginButton variant="contained" onClick={() => handleSubmit()}>Sign in</LoginButton>
+            </Grid>
+          </LoginForm>
+          <Grid container sx={{ height: '10rem' }}>
+            <Grid container>
+              <Grid item xs={4} sx={{ borderTop: '1px solid', height: '1px' }} />
+              <Grid item xs={4}>or</Grid>
+              <Grid item xs={4} />
+            </Grid>
+            <Grid item xs={12} sx={{ height: '100%' }}>
+              <Grid></Grid>
+            </Grid>
+          </Grid>
+        </LoginFormContainer>
+      </StyledLogin >
     );
-	},
+  },
 );
 
 export default Login;
