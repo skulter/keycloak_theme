1f6ed635a37754d5b682ded729b883ff4378019d
Kim JinHo <skulter@naver.com>
2022-06-21 18:05:25 +0900

    테스트

--- src/pages/keycloak/Login.tsx
+++ src/pages/keycloak/Login.tsx
@@ -1,13 +1,14 @@
-import React, { useState, useRef, memo, useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
+import { useRef, memo, useEffect } from 'react';
 import styled from 'styled-components';
-import { Box, Button, FormControl, Grid, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Typography } from '@mui/material';
+import { Button, FormControl, Grid, InputAdornment, InputLabel, OutlinedInput, TextField, Typography } from '@mui/material';
 import type { KcProps } from 'keycloakify/lib/components/KcProps';
 import type { KcContextType } from '@/utils/keycloakManager';
 import facebook from '@/assets/images/facebook.png';
 import google from '@/assets/images/google.png';
 import github from '@/assets/images/github.png';
 import microsoft from '@/assets/images/microsoft.png';
+import { Link } from 'react-router-dom';
+
 
 type KcContext_Login = Extract<KcContextType, { pageId: 'login.ftl' }>;
 
@@ -16,9 +17,9 @@ const StyledLogin = styled(Grid)`
   min-height: 100vh;
   background-color: rgb(222, 230, 240);
   display: flex;
-  flex-direction: column;
+  flex-direction: column !important;
   align-items: center;
-  justify-content: center;
+  justify-content: center !important;
 `;
 
 const LoginFormContainer = styled(Grid)`
@@ -27,15 +28,15 @@ const LoginFormContainer = styled(Grid)`
     /* border : 2px solid grey; */
     background-color: white;
     justify-content: center;
-    width: 30rem;
+    width: 23rem;
     /* height:35rem; */
     display: flex;
     flex-direction: column !important;
     align-items: center;
+    padding: 1rem 2rem !important;
 `
 
 const LoginForm = styled.form`
-  width: 25rem;
   margin: 2rem 0;
   display: flex;
   flex-direction: column;
@@ -55,11 +56,12 @@ const LoginButton = styled(Button)`
   width: 100%;
 `;
 
-const SocialForm = styled('a')`
-/* border: 1px solid grey; */
-border-radius: 30%;
-background-color: white;
-height:4rem;
+const Footer = styled.footer`
+  position: absolute;
+  bottom: 1rem;
+  display: flex;
+  flex-direction: column !important;
+  align-items: center !important;
 `
 
 const returnImg = (data: string): string => {
@@ -67,44 +69,68 @@ const returnImg = (data: string): string => {
     case "google": return google
     case "facebook": return facebook
     case "github": return github
-    case "microsoft": return microsoft
+    case "saml": return microsoft
     default: return google
   }
 }
 
+
+const year = new Date();
+
+
 export const Login = memo(
   ({ kcContext, ...props }: { kcContext: KcContext_Login } & KcProps) => {
-    const { t } = useTranslation();
     const form = useRef<HTMLFormElement>(null);
     const { social, url, message, realm, } = kcContext;
     const isSessionOut = message?.summary.includes('attempt timed out') || message?.summary.includes('Timeout');
-
     console.log(kcContext);
 
     const handleSubmit = () => {
       console.log(form);
       form?.current?.submit();
     };
+
     const test = {
-      displayInfo: true,
-      providers: [
+      "displayInfo": true,
+      "providers": [
         {
-          displayName: "Google",
-          providerId: "google",
-          loginUrl: "/auth/realms/test/broker/google/login?client_id=account-console&tab_id=0ncS40QMm5Q&session_code=OacQkf8KlbI6tefn7E5pDiTte7MTKHE5Qlenk1IWp5o",
-          iconClasses: "fa fa-google",
-          alias: "google"
+          "displayName": "GitHub",
+          "providerId": "github",
+          "loginUrl": "/realms/test-realm/broker/github/login?client_id=account-console&tab_id=PxLavC4stUQ&session_code=4ugPR7PppSRWJbkNaRFhDrnl45uP-2jEyrFzrpjjBao",
+          "iconClasses": "fa fa-github",
+          "alias": "github"
+        },
+        {
+          "displayName": "Microsoft",
+          "providerId": "saml",
+          "loginUrl": "/realms/test-realm/broker/Microsoft/login?client_id=account-console&tab_id=PxLavC4stUQ&session_code=4ugPR7PppSRWJbkNaRFhDrnl45uP-2jEyrFzrpjjBao",
+          "iconClasses": "",
+          "alias": "Microsoft"
+        },
+        {
+          "displayName": "Google",
+          "providerId": "google",
+          "loginUrl": "/realms/test-realm/broker/google/login?client_id=account-console&tab_id=PxLavC4stUQ&session_code=4ugPR7PppSRWJbkNaRFhDrnl45uP-2jEyrFzrpjjBao",
+          "iconClasses": "fa fa-google",
+          "alias": "google"
+        },
+        {
+          "displayName": "Facebook",
+          "providerId": "facebook",
+          "loginUrl": "/realms/test-realm/broker/facebook/login?client_id=account-console&tab_id=PxLavC4stUQ&session_code=4ugPR7PppSRWJbkNaRFhDrnl45uP-2jEyrFzrpjjBao",
+          "iconClasses": "fa fa-facebook",
+          "alias": "facebook"
         }
       ]
     }
 
     useEffect(() => {
       if (message?.summary === 'emailSentMessage') {
-        // toast.success(<Toast title={t('success.send.reset.password.email')} message={t('success.send.reset.password.email.default')} />);
+        toast.success(<Toast title={t('success.send.reset.password.email')} message={t('success.send.reset.password.email.default')} />);
       } else if (message?.summary === 'expiredActionTokenSessionExistsMessage') {
-        // toast.error(<Toast title={t('error.session.expired')} message={t('error.session.expired.default')} />);
+        toast.error(<Toast title={t('error.session.expired')} message={t('error.session.expired.default')} />);
       } else if (message?.summary === 'accountUpdatedMessage') {
-        // toast.success(<Toast title={t('success.account.update')} message={t('success.account.update.message')} />);
+        toast.success(<Toast title={t('success.account.update')} message={t('success.account.update.message')} />);
       }
     }, []);
 
@@ -115,7 +141,7 @@ export const Login = memo(
             marginTop: '3rem',
             justifyContent: 'center'
           }} >
-            <Typography variant='h4' >Login</Typography>
+            <Typography variant='h5' >User Login</Typography>
           </Grid>
           <LoginForm ref={form} method="post" action={url.loginAction}>
 
@@ -123,13 +149,13 @@ export const Login = memo(
               <Grid item>
                 <FormControl sx={{ marginBottom: 2, width: "100%" }} variant="outlined">
                   <InputLabel >
-                    username
+                    Email ID
                   </InputLabel>
                   <OutlinedInput
                     id="username"
                     name="username"
                     size="medium"
-                    label='username'
+                    label='Email ID'
                   />
                 </FormControl>
                 <FormControl sx={{ marginBottom: 2, width: "100%" }} variant="outlined">
@@ -141,7 +167,7 @@ export const Login = memo(
                     name="password"
                     type="password"
                     size="medium"
-                    label='password'
+                    label='Password'
                     endAdornment={
                       <InputAdornment position="end">
                         {/* <IconButton
@@ -159,27 +185,28 @@ export const Login = memo(
               </Grid>
               <LoginButton variant="contained" onClick={() => handleSubmit()}>Login</LoginButton>
             </Grid>
-          </LoginForm>
-          <Grid container direction='column' alignItems='center'>
-            <Grid item>
-              <Typography>or</Typography>
-            </Grid>
-            <Grid container alignContent='center'>
-              {/* <Grid item xs={3}>asd</Grid>
-              <Grid item xs={3}>asd</Grid>
-              <Grid item xs={3}>asd</Grid>
-              <Grid item xs={3}>asd</Grid> */}
-              <Box>
-                {test.providers?.map(provider => (
-                  <SocialForm key={provider.displayName} href={provider.loginUrl}>
+            <Grid container direction='column' alignItems='center' marginTop='2rem'>
+              <Grid item marginBottom='2rem'>
+                <Typography>or</Typography>
+              </Grid>
+              <Grid container alignContent='center' justifyContent='space-between'>
+                {test.displayInfo && test.providers?.map(provider => (
+                  // <Grid item xs={3} key={provider.displayName}>
+                  <a href={provider.loginUrl} key={provider.displayName}>
                     {/* {returnImg(provider.providerId)} */}
-                    <img width={30} src={returnImg(provider.providerId)} alt=""></img>
-                  </SocialForm>
+                    <img width={40} src={returnImg(provider.providerId)} alt={provider.providerId}></img>
+                  </a>
+                  // </Grid>
                 ))}
-              </Box>
+              </Grid>
             </Grid>
-          </Grid>
+          </LoginForm>
         </LoginFormContainer>
+        <Footer >
+          <Grid item><Typography>{year.getFullYear()} CLOUDMATE Corp. All Rights Reserved.</Typography></Grid>
+          <Grid item><a href='https://who.cloudmt.co.kr/privacypolicy' target='_blank' rel='noreferrer'><Typography>Privacy Policy</Typography></a></Grid>
+          <Grid item><Typography>Cookie Policy</Typography></Grid>
+        </Footer>
       </StyledLogin >
     );
   },
